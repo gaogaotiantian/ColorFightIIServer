285eccc35ebab253a6248032a7a13d31cb5edbb0
gaogaotiantian <gaogaotiantian@hotmail.com>
2019-02-03 11:37:46 -0800

    Changed the attribute name to match the info
    Add pytest to requirements

--- web/colorfight/building.py
+++ web/colorfight/building.py
@@ -5,11 +5,11 @@ class BaseBuilding:
     def __init__(self):
         self.level = 1
 
-    def get_energy_source(self, cell):
-        return cell.energy
+    def get_energy(self, cell):
+        return cell.natural_energy
 
-    def get_gold_source(self, cell):
-        return cell.gold
+    def get_gold(self, cell):
+        return cell.natural_gold
 
     def get_attack_cost(self, cell):
         return int(cell.natural_cost + cell.force_field)
@@ -25,10 +25,10 @@ class Empty(BaseBuilding):
 
 class Home(BaseBuilding):
     name = 'home'
-    def get_energy_source(self, cell):
+    def get_energy(self, cell):
         return 10
 
-    def get_gold_source(self, cell):
+    def get_gold(self, cell):
         return 10
 
     def get_attack_cost(self, cell):
@@ -38,15 +38,15 @@ class EnergyWell(BaseBuilding):
     name = "energy_well"
     cost = 100
 
-    def get_gold_source(self, cell):
-        return cell.energy * 2
+    def get_energy(self, cell):
+        return cell.natural_energy * 2
 
 class GoldMine(BaseBuilding):
     name = "gold_mine"
     cost = 100
 
-    def get_gold_source(self, cell):
-        return cell.gold * 2
+    def get_gold(self, cell):
+        return cell.natural_gold * 2
 
 def get_building_class(building):
     '''
