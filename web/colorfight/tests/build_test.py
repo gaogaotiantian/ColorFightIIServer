7fdb202186035818552566ee2c25a6c7b4c53ffe
Lele521 <tengyue@umail.ucsb.edu>
2019-04-21 17:52:25 -0700

    Add attack, upgrade tests,and improve build tests

--- web/colorfight/tests/build_test.py
+++ web/colorfight/tests/build_test.py
@@ -19,6 +19,26 @@ def test_build_on_base():
     expected_err_msg = 'b ' + str(x) + ' ' + str(y) + ' ' + 'g failed, There is a building on this cell already.'
     assert(info['error'][1][0] == expected_err_msg)
 
+def test_build_without_enough_resource():
+    game = Colorfight()
+    uid = join(game, 'a', 'a')
+    info = game.get_game_info()
+    x, y = info['users'][uid]['cells'][0]
+    game.users[uid].energy = 10000
+    game.users[uid].gold = 10
+    if y == 0:
+        attack_y = 1
+    else:
+        attack_y = y - 1
+    result = attack(game, uid, x, attack_y, 1000)
+    assert (result["success"])
+    game.update(True)
+    result = build(game, uid, x, attack_y, BLD_GOLD_MINE)
+    assert (result["success"])
+    game.update(True)
+    info = game.get_game_info()
+    assert (info['error'])
+
 def test_invalid_building_type():
     game = Colorfight()
     uid = join(game, 'a', 'a')
@@ -60,3 +80,9 @@ def test_build_on_invalid_cell():
     assert (info['error'])
     expected_err_msg = 'b ' + str(x) + ' ' + str(new_y) + ' ' + 'g failed, You need to build on your own cell.'
     assert (info['error'][1][0] == expected_err_msg)
+
+    result = build(game, uid, -1, -1, BLD_GOLD_MINE)
+    assert(result["success"])
+    game.update(True)
+    info = game.get_game_info()
+    assert(info["error"])
