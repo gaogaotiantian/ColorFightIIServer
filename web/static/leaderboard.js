d2b05a220546af4d2b076dbb8707d1cc517986bf
Tian Gao <gaogaotiantian@hotmail.com>
2019-05-19 15:32:14 -0700

    Cache leaderboard in session storage

--- web/static/leaderboard.js
+++ web/static/leaderboard.js
@@ -21,22 +21,36 @@ function createUserDiv(rank, username, score) {
 }
 
 function draw_leaderboard() {
-    var arr = [];
-    var ref = firebase.database().ref('/leaderboard').orderByChild('score').limitToLast(100);
-    ref.once('value', function(snapshot) {
-        snapshot.forEach(function(child) {
-            arr.push([child.key, child.val()]);
+    let curr_time = new Date().getTime();
+    let expire_time = 5*60*1000;
+    let arr = [];
+    if (sessionStorage.getItem('leaderboard_data') && 
+            parseFloat(sessionStorage.getItem('leaderboard_data_timestamp')) > curr_time - expire_time) {
+        arr = JSON.parse(sessionStorage.getItem('leaderboard_data'));
+        draw_leaderboard_with_data(arr);
+    } else {
+        var ref = firebase.database().ref('/leaderboard').orderByChild('score').limitToLast(100);
+        ref.once('value', function(snapshot) {
+            snapshot.forEach(function(child) {
+                arr.push([child.key, child.val()]);
+            })
+            draw_leaderboard_with_data(arr);
+            sessionStorage.setItem('leaderboard_data', JSON.stringify(arr));
+            sessionStorage.setItem('leaderboard_data_timestamp', curr_time);
         })
-        let container = document.getElementById('leaderboard-content');
-        container.innerHTML = "";
-        for (let idx = arr.length-1; idx >= 0; idx --) {
-            let user = arr[idx];
-            let username = user[0];
-            let userdata = user[1];
-            let rank = arr.length - idx;
-            container.appendChild(createUserDiv(rank, username, userdata['score'].toFixed(2)));
-        }
-    })
+    }
+}
+
+function draw_leaderboard_with_data(arr) {
+    let container = document.getElementById('leaderboard-content');
+    container.innerHTML = "";
+    for (let idx = arr.length-1; idx >= 0; idx --) {
+        let user = arr[idx];
+        let username = user[0];
+        let userdata = user[1];
+        let rank = arr.length - idx;
+        container.appendChild(createUserDiv(rank, username, userdata['score'].toFixed(2)));
+    }
 }
 
 $(function() {
